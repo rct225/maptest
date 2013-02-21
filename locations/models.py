571032d407313df90370f1038404db510272263a
Rob Tuck <robtuck@gmail.com>
2013-02-21 17:58:38 -0500

    backend list of locations is working

--- locations/models.py
+++ locations/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Locations(models.Model):
+    locationName = models.CharField(max_length=200, blank=True)
+    latitude = models.CharField(max_length=25, blank=True)
+    longitute = models.CharField(max_length=25, blank=True)
+    locationAddress = models.CharField(max_length=55, blank=True)
+    
--- locations/views.py
+++ locations/views.py
@@ -1,1 +1,13 @@
 # Create your views here.
+from django.http import HttpResponse
+from django.template import RequestContext
+from django.core import serializers
+from locations.models import Locations
+
+def index(request):
+    return HttpResponse("Hello World!")
+
+def locationJSON(request):
+    locations = Locations.objects.all()
+    data = serializers.serialize('json', locations)
+    return HttpResponse(data, mimetype='application/json')
