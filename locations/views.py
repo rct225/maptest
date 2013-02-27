589d7ed0a16cdf570bfc8b22cfc37d7440024ecf
Rob Tuck <robtuck@gmail.com>
2013-02-26 19:27:35 -0500

    tweaked JSON

--- locations/views.py
+++ locations/views.py
@@ -1,13 +1,21 @@
 # Create your views here.
 from django.http import HttpResponse
-from django.template import RequestContext
-from django.core import serializers
 from locations.models import Locations
+from serializers import ModelSerializer
 
-def index(request):
-    return HttpResponse("Hello World!")
 
 def locationJSON(request):
     locations = Locations.objects.all()
-    data = serializers.serialize('json', locations)
+    data = LocationSerializer().serialize('json', locations)
     return HttpResponse(data, mimetype='application/json')
+
+class LocationSerializer(ModelSerializer):
+    class Meta:
+        model = Locations
+        exclude = ('id',)
+        nested = 1
+        
+def index(request):
+    return HttpResponse("Hello World!")
+
+
